e44902c4a46594079f33e3ef4a5b791f0bc7081a
Mathias <mroe734@aucklanduni.ac.nz>
2025-03-18 07:34:35 +1300

    Updated the ortho writing script to include angles at the end of the line if needed

--- code/MATLAB/structure_tensor/WriteOrthoFile.m
+++ code/MATLAB/structure_tensor/WriteOrthoFile.m
@@ -1,14 +1,18 @@
-function WriteOrthoFile(file_name, fibres, sheets, normals)
+function WriteOrthoFile(file_name, fibres, sheets, normals, fibre_angles)
 %WRITEORTHOFILE Writes the ortho file based on fibres, sheets, and normals
 %vectors.
 %
 %   Args:
 %    - file_name, path to the ortho file to write.
 %    - fibres, fibre orientation vectors fibres(N, 3).
-%    - sheets, sheet orientation vectors fibres(N, 3).
-%    - normals, normal orientation vectors fibres(N, 3).
+%    - sheets, sheet orientation vectors sheets(N, 3).
+%    - normals, normal orientation vectors normals(N, 3).
+%    - fibre_angles, angles relative to the centreline fibre_angles(N, 1).
 %
 %   Return:
+if nargin < 5
+    fibre_angles = [];
+end
 %% Check the fibres, sheets, and normals have the same size
 if ~all(size(fibres) == size(sheets))
     error("Error: fibres and sheets should have the same size.")
@@ -23,7 +27,10 @@ fprintf(fid,'%d\n', length(fibres));  % Print number of elements
 for i = 1:length(fibres)
     fprintf(fid, '%f %f %f', fibres(i, 1), fibres(i, 2), fibres(i, 3));
     fprintf(fid, ' %f %f %f ', sheets(i, 1), sheets(i, 2), sheets(i, 3));
-    fprintf(fid, '%f %f %f\n', normals(i, 1), normals(i, 2), normals(i, 3));
+    fprintf(fid, '%f %f %f ', normals(i, 1), normals(i, 2), normals(i, 3));
+    if length(fibre_angles) ~= 0
+        fprintf(fid, '%f\n', fibre_angles(i));
+    end
 end
 
 fclose(fid);
