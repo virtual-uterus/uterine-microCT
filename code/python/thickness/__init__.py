7f5bd332b287605443f63e03ee9395ae18b807ec
Mathias <mroe734@aucklanduni.ac.nz>
2025-03-21 14:34:05 +1300

    Updated versions

---
+++ code/python/thickness/__init__.py
@@ -0,0 +1,15 @@
+"""
+thickness
+========
+A Python package for manipulating uterine uCT dataset and
+evaluate the thickness of the muscle layers.
+
+Modules:
+- thickness: Functions used to calculate thickness.
+- plots: Plotting functions.
+- utils: General utilities for handling data and file operations.
+- constants: Constants used in the package.
+
+"""
+
+__version__ = "0.1.1"
