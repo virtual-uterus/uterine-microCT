fe8795c1e5cb184777765f0444cd4da6c7bcae67
Mathias <mroe734@aucklanduni.ac.nz>
2025-08-13 11:56:10 +1200

    Wrote a script to estimate endometrium volume based on segmentation masks

---
+++ code/python/scripts/endometrium-analysis.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+endometrium-analysis.py
+
+Script to analyse endometrium volume in uterine horns
+Author: Mathias Roesler
+Date: 07/25
+"""
+
+import argparse
+import os
+import pickle
+
+import numpy as np
+import scipy.io
+
+import thickness.plots as plots
+import thickness.projection as projection
+import thickness.utils as utils
+
+from thickness.constants import BASE, HOME
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Determines the volume of the endometrium from uCT data"
+    )
+
+    parser.add_argument(
+        "dir_path",
+        type=str,
+        metavar="dir-path",
+        help="path from BASE to the dataset",
+    )
+    parser.add_argument(
+        "base_name", type=str, metavar="base-name", help="name of the dataset"
+    )
+    parser.add_argument(
+        "-e",
+        "--extension",
+        type=str,
+        metavar="extension",
+        help="extension for the saved images, default png",
+        default="png",
+    )
+    parser.add_argument(
+        "--horn",
+        type=str,
+        choices={"left", "right", "both"},
+        help="horn to process",
+        default="both",
+    )
+    parser.add_argument(
+        "-s",
+        "--switch",
+        action="store_true",
+        help="switches the labels of the left and right horn, default False",
+    )
+    parser.add_argument(
+        "--not-d",
+        action="store_true",
+        help="flag used if the dataset is not downsampled, default False",
+    )
+
+    # Parse input arguments
+    args = parser.parse_args()
+
+    load_directory = os.path.join(
+        HOME,
+        BASE,
+        args.dir_path,
+        args.base_name,
+    )
+
+    if not args.not_d:
+        # If the dataset is downsampled
+        load_directory = os.path.join(load_directory, "downsampled")
+        param_file = os.path.join(
+            load_directory,
+            args.base_name + "_downsampled.toml",
+        )
+
+    else:
+        # If not use top-level parameter file
+        param_file = os.path.join(load_directory, args.base_name + ".toml")
+
+    # Load parameters
+    params = utils.parse_TOML(param_file)
+    split_nb = params["split_nb"]  # Get horn separation slice
+    weight = params["weight"] * 1e-3  # Weight in mg for normalisation
+    resolution = params["resolution"] * 1e-3  # In mm
+
+    # Add the endometrium segmentation to the load directory
+    load_directory = os.path.join(load_directory, "endometrium_segmentation")
+
+    # Convert both to left and right
+    if args.horn == "both":
+        horns = ["left", "right"]
+
+    else:
+        horns = [args.horn]
+
+    volume_dict = dict()
+
+    for i, horn in enumerate(horns):
+        if args.switch:
+            print_horn = horns[i - 1]
+
+        else:
+            print_horn = horn
+
+        print("Processing {} horn".format(print_horn))
+        print("   Loading mask stack")
+        mask_stack = utils.load_image_stack(
+            os.path.join(load_directory, "{}".format(horn)),
+            extension=args.extension,
+        )
+
+        nb_endo_pixels = np.sum(mask_stack[:, :, split_nb:] >= 1)
+        endometrium_volume = nb_endo_pixels * (resolution**3)
+        volume_dict[print_horn] = endometrium_volume / weight
+
+        print(
+            "{} horn endometrium volume: {:.2f} mm3/mg".format(
+                print_horn,
+                endometrium_volume / weight,
+            )
+        )
+
+    with open(load_directory + "/endometrium_volume.pkl", "wb") as f:
+        pickle.dump(volume_dict, f)
